8ea8ee02ccdf2fec55cf03fb7924839ee36fb69c
Tony Faieta <tfaieta@gmail.com>
2018-09-15 02:01:13 -0400

    initial commit

---
+++ app/components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+  render() {
+    return (
+    <div>
+        <h1>
+          Welcome to DarkPDF.
+        </h1>
+    </div>
+    )
+  }
+}
