495a3fc4702a01b20047ecd45e9e81e11aff084d
Tony Faieta <tfaieta@gmail.com>
2018-09-15 04:14:44 -0400

    file

--- app/components/FileReader.js
+++ app/components/FileReader.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Document } from 'react-pdf'
+import { Document, Page } from 'react-pdf';
 
 export default class FileReader extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
   render() {
+    const { pageNumber, numPages } = this.state;
+
     return (
       <div>
-        
+        <Document
+          file="sample.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
       </div>
-    )
-  }
+    );
 }
