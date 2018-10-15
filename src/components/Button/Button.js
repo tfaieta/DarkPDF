1056e647854c47009c8f5eac56de1cac0e9c96a0
Tony Faieta <tfaieta@gmail.com>
2018-10-15 00:33:37 -0400

    Updated default

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,7 +6,7 @@ export default class Button extends Component {
   render() {
     return (
       <div style={{justifyContent: "center", alignItems: "center",  marginTop: 35}}>
-        <button> 
+        <button autoFocus> 
             <div className="Example__container__load">
                 <Icon type={this.props.type} style={{ fontSize: '25px', color: '#08c'}}>
                 </Icon>
