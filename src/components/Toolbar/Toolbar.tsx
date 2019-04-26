e69fd9f94473d83bdb9259a29c9b2764bef6c115
Tony Faieta <tfaieta@gmail.com>
2019-04-26 19:54:45 -0400

    added antd + icons to toolbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 import styled from "styled-components";
+import { Icon } from "antd";
 
 interface ToolbarProps {}
 
 const Toolbar: React.SFC<ToolbarProps> = props => {
-  return <Sidebar />;
+  return (
+    <Sidebar>
+      <IconList>
+        {icons.map((icon, index) => {
+          return <StyledIcon type={icon} key={index} />;
+        })}
+      </IconList>
+    </Sidebar>
+  );
 };
 
 export default Toolbar;
@@ -17,4 +26,17 @@ const Sidebar = styled.div`
   top: 0;
   background: #0f111a;
   position: absolute;
+  padding-top: 30px;
 `;
+
+const IconList = styled.div`
+  display: grid;
+`;
+
+const StyledIcon = styled(Icon)`
+  font-size: 25px;
+  color: #08c;
+  margin: 15px 0;
+`;
+
+const icons = ["zoom-in", "zoom-out", "book", "github"];
